b7525fedac8f838dfb7a8484cc9b0378bf71edab
root <theophile.poumeyrol@gmail.com>
2023-11-13 21:48:21 +0100

    ajout template admin et public

--- app/views.py
+++ app/views.py
@@ -5,3 +5,7 @@ from flask import render_template
 @app.route("/")
 def index():
     return render_template("public/index.html")
+
+@app.route("/public/a-propos")
+def public_a_propos():
+    return render_template("public/a-propos.html")
