7a8ce20025b49fa19fa78857ed7b744157d69413
root <theophile.poumeyrol@gmail.com>
2023-11-14 06:34:00 +0100

    creation error pages

--- app/views.py
+++ app/views.py
@@ -9,3 +9,13 @@ def index():
 @app.route("/public/a-propos")
 def public_a_propos():
     return render_template("public/a-propos.html")
+
+# Page not found
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error_pages/404.html"), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("error_pages/404.html"), 500
